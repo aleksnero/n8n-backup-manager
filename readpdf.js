1fb35019c2108a07019b3634f47a5de29f5a2ba8
aleksnero <aleksnero.app@outlook.it>
2026-09-06 13:03:50 +0200

    chore: remove temporary report and scratch script from tracking

--- readpdf.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const pdf = require('pdf-parse');
-
-const dataBuffer = fs.readFileSync('C:\\Users\\alex\\Desktop\\Bulletproof_Automation_QA_Client_Report_n8n_Backup_Manager_2026-05-11.pdf');
-
-pdf(dataBuffer).then(function(data) {
-    console.log(data.text);
-}).catch(err => console.error(err));
